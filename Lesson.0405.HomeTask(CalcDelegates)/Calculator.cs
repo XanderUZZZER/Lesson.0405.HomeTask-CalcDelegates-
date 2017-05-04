38a25287ddaeee9e93ab84886dda15bf1e009545
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-05-04 17:01:46 +0300

    0405171701

---
+++ Lesson.0405.HomeTask(CalcDelegates)/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson._0405.HomeTask_CalcDelegates_
+{
+    class Calculator
+    {
+        //private delegate double OperationDelegate(double x, double y);
+        private static Dictionary<char, Func<double, double, double>> operations =
+                        new Dictionary<char, Func<double, double, double>>
+                            {
+                                { '+', (x, y) => x + y },
+                                { '-', (x, y) => x - y },
+                                { '*', (x, y) => x * y },
+                                { '/', (x, y) => x / y },
+                            };
+        public static double PerformOperation(char operation, double x, double y)
+        {
+            if (!operations.ContainsKey(operation))
+                throw new ArgumentException(string.Format($"Operation {operation} is invalid"), "op");
+            return operations[operation](x, y);
+        }
+    }
+}
--- Lesson.0405.HomeTask(CalcDelegates)/Program.cs
+++ Lesson.0405.HomeTask(CalcDelegates)/Program.cs
@@ -10,6 +10,22 @@ namespace Lesson._0405.HomeTask_CalcDelegates_
     {
         static void Main(string[] args)
         {
+            double num1, num2, result;
+            char op;
+            string[] temp;
+
+            Console.WriteLine("Lesson 0305 HomeTask\n");
+            Console.WriteLine("Input 2 numbers and operator to perform a calulation:\n");
+            temp = Console.ReadLine().Split(' ');
+
+            double.TryParse(temp[0], out num1);
+            double.TryParse(temp[1], out num2);
+            char.TryParse(temp[2], out op);
+
+            result = Calculator.PerformOperation(op, num1, num2);
+            Console.WriteLine($"Result is: {result}");
+
+            Console.ReadLine();
         }
     }
 }
