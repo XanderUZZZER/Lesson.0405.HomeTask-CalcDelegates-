519e485b059fa72ab0330e489ecea614d00993cc
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-05-04 16:31:01 +0300

    Add project files.

---
+++ Lesson.0405.HomeTask(CalcDelegates)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson._0405.HomeTask_CalcDelegates_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
